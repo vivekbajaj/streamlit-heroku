c0ae9e6b0d8f8880ad4599cfa4caa3095be6a94d
vivekbajaj <vivekbajaj@users.noreply.github.com>
2022-11-23 23:49:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+st.write('# Sum of Two Numbers')
+#get input
+# input 1
+num1 = st.number_input(label="Enter first number")
+# input 2
+num2 = st.number_input(label="Enter second number") 
+
+ans = 0
+def calculate():
+    ans = num1 + num2
+    st.success(f"Answer = {ans}")
+if st.button("Calculate result"):
+    calculate()
