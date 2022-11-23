648a611698a35257388748e7c741607241839623
vivekbajaj <vivekbajaj@users.noreply.github.com>
2022-11-24 00:20:10 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"vivekbajaj@hotmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
