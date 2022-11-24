a1197faa8615fd7f56ce15eaef625f58e33ec60d
vivekbajaj <vivekbajaj@users.noreply.github.com>
2022-11-25 00:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+echo "# streamlit-to-heroku-tutorial" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/your_username/your_repo_name.git
+git push -u origin master
+
 import streamlit as st
 st.write('# Sum of Two Numbers')
 #get input
